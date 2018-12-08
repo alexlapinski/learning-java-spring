a88b9d8f739566b5844ef415abd6731c433848e2
Alex Lapinski <contact@alexlapinski.name>
2018-12-08 12:13:48 -0500

    Created initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spring Boot Example
+
+A very small project built to learn Spring Boot.
+
+## Build
+```
+gradle build
+```
---
+++ src/main/java/hello/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package hello.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import hello.models.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(path = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="@orld") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+    }
+
+}
